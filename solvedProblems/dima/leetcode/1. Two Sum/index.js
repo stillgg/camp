d4303485a73e6899e73c335a32bff1b2e34c7be3
mar1cruz <ddovidovich@list.ru>
2023-05-23 21:13:29 +0300

    https://github.com/stillgg/camp/issues/5

---
+++ solvedProblems/dima/leetcode/1. Two Sum/index.js
@@ -0,0 +1,24 @@
+function twoSum(nums, target) {
+  let summ = 0;
+  const arrIndex = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    summ = nums[i];
+
+    for (let j = 0; j < nums.length; j++) {
+      if (i === j) {
+        continue;
+      } else if (summ + nums[j] === target) {
+        arrIndex.push(i);
+        arrIndex.push(j);
+        summ += nums[j];
+        break;
+      }
+    }
+
+    if (summ === target) break;
+  }
+  return arrIndex;
+}
+
+twoSum([3, 1, 2, 2, 8, 4], 11);
