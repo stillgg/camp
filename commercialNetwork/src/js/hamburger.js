44388bcbdf80f6bfbaed58a1467dc91f605d948d
Makcik303123 <adoberoflit@gmail.com>
2023-05-29 04:32:05 +0300

    https://github.com/stillgg/camp/issues/6 for review

---
+++ commercialNetwork/src/js/hamburger.js
@@ -0,0 +1,17 @@
+const hamburgerBtn = document.querySelector("#main__icon");
+const hamburgerBody = document.querySelector("#menu__body");
+const body = document.querySelector("body");
+
+hamburgerBtn.addEventListener("click", () => {
+  hamburgerBtn.classList.toggle("active");
+  hamburgerBody.classList.toggle("active");
+  body.classList.toggle("lock");
+});
+
+hamburgerBody.querySelectorAll(".nav__link").forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburgerBtn.classList.remove("active");
+    hamburgerBody.classList.remove("active");
+    body.classList.remove("lock");
+  });
+});
