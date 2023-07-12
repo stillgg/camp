f963264ad70310cb1335144cb5c43d7e6867e16f
Makcik303123 <adoberoflit@gmail.com>
2023-07-12 22:27:04 +0300

    https://github.com/stillgg/camp/issues/68 add setTimeout

--- commercialNetwork/src/js/scrollBar.js
+++ commercialNetwork/src/js/scrollBar.js
@@ -5,37 +5,50 @@ const scrollbar = document.querySelector(".scrollbar");
 const main = document.querySelector(".main");
 let activeSlide = 0;
 let position = 0;
+let start = true;
 const blackSection = [2, 6, 7, 8, 10, 11];
 
 addEventListener("wheel", (event) => checkSection(event));
 
 function checkSection(event) {
-  const arrTopPosition = [];
+  if (start === true) {
+    const arrTopPosition = [];
 
-  for (let i = 0; i < sections.length; i++) {
-    arrTopPosition.push(sections[i].offsetHeight * i);
+    for (let i = 0; i < sections.length; i++) {
+      arrTopPosition.push(sections[i].offsetHeight * i);
+    }
+
+    if (event.deltaY > 0 && activeSlide !== 11 && start === true) {
+      activeSlide++;
+    }
+
+    if (event.deltaY < 0 && activeSlide !== 0 && start === true) {
+      activeSlide--;
+    }
+
+    start = false;
+    position = arrTopPosition[activeSlide];
+    main.style.transform = `translateY(${-position}px)`;
+    changeCounter(activeSlide);
+    changeLink(activeSlide);
+    scrollBarColor(activeSlide);
+    console.log(activeSlide);
+    setTimeout(startF, 500);
   }
+}
 
-  if (event.deltaY > 0 && activeSlide !== 11) {
-    activeSlide++;
-  }
-
-  if (event.deltaY < 0 && activeSlide !== 0) {
-    activeSlide--;
-  }
-
-  position = arrTopPosition[activeSlide];
-  main.style.transform = `translateY(${-position}px)`;
-  changeCounter(activeSlide);
-  changeLink(activeSlide);
-  scrollBarColor(activeSlide);
+function startF() {
+  start = true;
+  console.log("Srabotalo");
 }
 
 function changeCounter(index) {
+  console.log("changeCounter");
   counter.innerHTML = index + 1 >= 10 ? index + 1 : "0" + (index + 1);
 }
 
 function changeLink(index) {
+  console.log("changeLink");
   links.forEach((link) => {
     link.classList.remove("active");
   });
@@ -43,6 +56,7 @@ function changeLink(index) {
 }
 
 function scrollBarColor(index) {
+  console.log("scrollBarColor");
   blackSection.includes(index)
     ? scrollbar.classList.add("black")
     : scrollbar.classList.remove("black");
