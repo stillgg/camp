c1d76292b250088b0117429b5c922b29142d5a3d
Maksim <adoberoflit@gmail.com>
2023-07-19 13:30:52 +0300

    https://github.com/stillgg/camp/issues/76 black header

--- commercialNetwork/src/js/scrollBar.js
+++ commercialNetwork/src/js/scrollBar.js
@@ -4,6 +4,7 @@ const indicator = document.querySelector("#indicator");
 const indicatorLines = indicator.querySelectorAll(".sections__wrapper");
 const counter = document.querySelector(".number__current");
 const main = document.querySelector(".main");
+const header = document.querySelector(".header");
 
 const BLACK_SECTION_INDEXES = [2, 6, 7, 8, 10, 11];
 
@@ -13,102 +14,105 @@ let timeoutId;
 let clientY1;
 
 const watchedSlide = new Proxy(
-  {
-    activeSlide: 0,
-  },
-  onSlideChange()
+	{
+		activeSlide: 0,
+	},
+	onSlideChange()
 );
 
 function onSlideChange() {
-  return {
-    set(target, property, slideIndex) {
-      if (slideIndex < 0 || slideIndex > 11) {
-        return;
-      }
+	return {
+		set(target, property, slideIndex) {
+			if (slideIndex < 0 || slideIndex > 11) {
+				return;
+			}
 
-      const sectionHeight = document.documentElement.clientHeight;
+			const sectionHeight = document.documentElement.clientHeight;
 
-      counter.innerText = slideIndex + 1;
+			counter.innerText = slideIndex + 1;
 
-      main.style.transform = `translateY(${-(sectionHeight * slideIndex)}px)`;
+			main.style.transform = `translateY(${-(sectionHeight * slideIndex)}px)`;
 
-      sections.forEach((section) => section.classList.remove("active"));
-      indicatorLines.forEach((line) => line.classList.remove("active"));
-      indicator.classList.remove("black");
+			sections.forEach((section) => section.classList.remove("active"));
+			indicatorLines.forEach((line) => line.classList.remove("active"));
+			indicator.classList.remove("black");
+			header.classList.remove("black");
 
-      sections[slideIndex].classList.add("active");
-      indicatorLines[slideIndex].classList.add("active");
+			sections[slideIndex].classList.add("active");
+			indicatorLines[slideIndex].classList.add("active");
 
-      if (BLACK_SECTION_INDEXES.includes(slideIndex))
-        indicator.classList.add("black");
+			if (BLACK_SECTION_INDEXES.includes(slideIndex)) {
+				indicator.classList.add("black");
+				header.classList.add("black");
+			}
 
-      return Reflect.set(target, property, slideIndex);
-    },
-  };
+			return Reflect.set(target, property, slideIndex);
+		},
+	};
 }
 
 indicatorLines.forEach((line, index) =>
-  line.addEventListener("click", (e) => {
-    e.stopPropagation();
-    watchedSlide.activeSlide = index;
-  })
+	line.addEventListener("click", (e) => {
+		e.stopPropagation();
+		watchedSlide.activeSlide = index;
+	})
 );
 
 function delay() {
-  timer = setTimeout(() => {
-    clearTimeout(timer);
-    timer = null;
-  }, 500);
+	timer = setTimeout(() => {
+		clearTimeout(timer);
+		timer = null;
+	}, 500);
 }
 
 function onWheel(e) {
-  clearTimeout(timeoutId);
+	clearTimeout(timeoutId);
 
-  const scrollAmountY = e.deltaY;
+	const scrollAmountY = e.deltaY;
 
-  const isScrollDown = scrollAmountY > 50;
-  const isScrollUp = scrollAmountY < -50;
+	const isScrollDown = scrollAmountY > 50;
+	const isScrollUp = scrollAmountY < -50;
 
-  if (isStart === false && timer === null) {
-    if (isScrollDown) {
-      isStart = true;
-      watchedSlide.activeSlide++;
+	if (isStart === false && timer === null) {
+		if (isScrollDown) {
+			isStart = true;
+			watchedSlide.activeSlide++;
 
-      delay();
-    }
+			delay();
+		}
 
-    if (isScrollUp) {
-      isStart = true;
-      watchedSlide.activeSlide--;
+		if (isScrollUp) {
+			isStart = true;
+			watchedSlide.activeSlide--;
 
-      delay();
-    }
-  }
+			delay();
+		}
+	}
 
-  timeoutId = setTimeout(function () {
-    isStart = false;
-  }, 25);
+	timeoutId = setTimeout(function () {
+		isStart = false;
+	}, 25);
 }
 
 function onDragStart(e) {
-  clientY1 = e.touches ? e.touches[0].clientY : e.clientY;
+	clientY1 = e.touches ? e.touches[0].clientY : e.clientY;
 }
 
 function onDragEnd(e) {
-  const clientY2 = e.changedTouches ? e.changedTouches[0].clientY : e.clientY;
-  diff = clientY1 - clientY2;
+	const clientY2 = e.changedTouches ? e.changedTouches[0].clientY : e.clientY;
+	diff = clientY1 - clientY2;
 
-  if (diff < -100) {
-    watchedSlide.activeSlide--;
-  }
-  if (diff > 100) {
-    watchedSlide.activeSlide++;
-  }
+	if (diff < -100) {
+		watchedSlide.activeSlide--;
+	}
+	if (diff > 100) {
+		watchedSlide.activeSlide++;
+	}
 }
 
-document.addEventListener("wheel", onWheel);
-document.addEventListener("touchstart", onDragStart);
-document.addEventListener("touchend", onDragEnd);
+main.addEventListener("wheel", onWheel);
+main.addEventListener("touchstart", onDragStart);
+main.addEventListener("touchend", onDragEnd);
 
-document.addEventListener("mousedown", onDragStart);
-document.addEventListener("mouseup", onDragEnd);
+main.addEventListener("mousedown", onDragStart);
+main.addEventListener("mouseup", onDragEnd);
