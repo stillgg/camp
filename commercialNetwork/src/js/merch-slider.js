3a96b370c87c6f09e1b6d163889f1dc5b19747a9
mar1cruz <ddovidovich@list.ru>
2023-07-20 19:49:33 +0300

    https://github.com/stillgg/camp/issues/69 fix slider

--- commercialNetwork/src/js/merch-slider.js
+++ commercialNetwork/src/js/merch-slider.js
@@ -1,9 +1,12 @@
 const btnNext = document.querySelector("#btn-next");
 const btnPrev = document.querySelector("#btn-prev");
+const section = document.querySelector(".merch");
 
 const track = document.querySelector(".slider-track");
 const items = document.querySelectorAll(".slider-item");
 const getGap = getComputedStyle(track);
+
+const zdvig = shiftCalculate();
 
 let indexActiveSlide = 0;
 
@@ -11,12 +14,12 @@ if (indexActiveSlide === 0) btnPrev.classList.add("hiddenBtn-left");
 
 btnNext.addEventListener("click", () => {
   const clientWidth = window.innerWidth;
-  const shiftValue = shiftCalculate();
+
   removeIndex = clientWidth >= 993 ? 2 : 1;
 
   if (Math.abs(indexActiveSlide) < items.length - removeIndex) {
     indexActiveSlide--;
-    track.style.transform = shift(indexActiveSlide, shiftValue);
+    track.style.transform = shift(indexActiveSlide, zdvig);
 
     if (
       (clientWidth >= 993 && indexActiveSlide === -2) ||
@@ -30,11 +33,9 @@ btnNext.addEventListener("click", () => {
 });
 
 btnPrev.addEventListener("click", () => {
-  const shiftValue = shiftCalculate();
-
   if (indexActiveSlide !== 0) {
     indexActiveSlide++;
-    track.style.transform = shift(indexActiveSlide, shiftValue);
+    track.style.transform = shift(indexActiveSlide, zdvig);
 
     if (indexActiveSlide === 0) btnPrev.classList.add("hiddenBtn-left");
 
@@ -45,6 +46,7 @@ btnPrev.addEventListener("click", () => {
 });
 
 function shift(index, shift) {
+  // console.log("index - ", index, "shift - ", shift);
   return `translateX(${index * shift}px)`;
 }
 
@@ -53,96 +55,65 @@ function shiftCalculate() {
   return (shiftValue = parseInt(getGap.gap) + itemWidth);
 }
 
-let startPos = 0;
+let positionStart = 0;
+let currentPosition = 0;
+
 let isDrag = false;
-let currentTranslate = 0;
-let prevTranslate = 0;
 
-track.addEventListener("touchstart", dragStart);
-track.addEventListener("touchmove", drag);
-track.addEventListener("touchend", dragEnd);
-
-function dragStart(event) {
+function onDragStart(event) {
   event.preventDefault();
 
-  if (event.type === "touchstart") {
-    startPos = event.touches[0].clientX;
-    isDrag = true;
-  }
+  isDrag = true;
+
+  positionStart =
+    event.type === "touchstart" ? event.touches[0].clientX : event.clientX;
+
+  positionStart += currentPosition;
 }
 
-function drag(event) {
+function onDragOver(event) {
   if (!isDrag) return;
 
-  if (event.type === "touchmove") {
-    const currentPosition = event.touches[0].clientX;
-
-    currentTranslate = prevTranslate + (currentPosition - startPos) * 1.5;
-    shiftValue = shiftCalculate();
-
-    if (
-      currentTranslate > 0 ||
-      currentTranslate < -((items.length - 1) * shiftValue)
-    ) {
-      currentTranslate = prevTranslate;
-      return;
-    }
-    track.style.transform = `translateX(${currentTranslate}px)`;
-  }
+  const move =
+    event.type === "touchmove" ? event.touches[0].clientX : event.clientX;
+  console.log(move);
+  track.style.transform = `translateX(${move - positionStart}px)`;
 }
 
-function dragEnd() {
+function onDragEnd(event) {
   isDrag = false;
-  prevTranslate = currentTranslate;
+
+  let positionEnd =
+    event.type === "touchend" ? event.changedTouches[0].clientX : event.clientX;
+
+  positionEnd += currentPosition;
+
+  console.log(positionEnd);
+
+  if (
+    positionStart > positionEnd &&
+    Math.abs(positionEnd - positionStart) > zdvig * 0.2 &&
+    -indexActiveSlide !== items.length - 1
+  ) {
+    indexActiveSlide--;
+  }
+
+  if (
+    positionStart < positionEnd &&
+    positionEnd - positionStart > zdvig * 0.2 &&
+    indexActiveSlide !== 0
+  ) {
+    indexActiveSlide++;
+  }
+
+  track.style.transform = shift(indexActiveSlide, zdvig);
+  currentPosition = -(indexActiveSlide * zdvig);
 }
 
-// let position = 0;
-// const slidesToShow = 2;
-// const slidesToScroll = 1;
+track.addEventListener("mousedown", onDragStart);
+section.addEventListener("mousemove", onDragOver);
+section.addEventListener("mouseup", onDragEnd);
 
-// const container = document.querySelector(".slider-container");
-// const track = document.querySelector(".slider-track");
-// // const item = document.querySelector(".merch-item");
-
-// const items = document.querySelectorAll(".slider-item");
-// const itemsCount = items.length;
-// const itemWitdh = container.clientWidth / slidesToShow;
-// const movePosition = slidesToScroll * itemWitdh;
-
-// const zdvig = container.clientWidth * 0.48;
-
-// items.forEach((item) => {
-//   // item.style.minWidth = `${48}%`;
-// });
-
-// btnNext.addEventListener("click", () => {
-//   const itemsLeft =
-//     itemsCount - (Math.abs(position) + slidesToShow * itemWitdh) / itemWitdh;
-
-//   position -=
-//     itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWitdh;
-
-//   setPosition();
-//   checkBtns();
-// });
-
-// btnPrev.addEventListener("click", () => {
-//   const itemsLeft = Math.abs(position) / itemWitdh;
-
-//   position +=
-//     itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWitdh;
-
-//   setPosition();
-//   checkBtns();
-// });
-
-// const setPosition = () => {
-//   track.style.transform = `translateX(${position}px)`;
-// };
-
-// const checkBtns = () => {
-//   btnPrev.disabled = position === 0;
-//   btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWitdh;
-// };
-
-// checkBtns();
+track.addEventListener("touchstart", onDragStart);
+section.addEventListener("touchmove", onDragOver);
+section.addEventListener("touchend", onDragEnd);
