1cc564430a06fed060705d486f0798f2af40d731
Makcik303123 <adoberoflit@gmail.com>
2023-08-03 21:01:30 +0300

    complete

--- commercialNetwork/src/js/networkLoader.js
+++ commercialNetwork/src/js/networkLoader.js
@@ -4,22 +4,16 @@ const numbers = network.querySelectorAll(".number__loader")
 
 function animationNetwork() {
   numbers.forEach((number, index) => {
-    let timer
     let i = 1
     const num = number.dataset.num
     const time = 4
     const animationNum = Number(num.split("").splice(0, 3).join(""))
-    const ostatok = num.split("").splice(3, num.length)
+    const staticNum = num.split("").splice(3, num.length)
     const step = (1000 * time + index * 200) / animationNum
-    if (timer !== undefined) {
-      console.log("обнуление")
-      console.log(timer)
-      clearInterval(timer)
-    }
-    console.log(timer)
-    timer = setInterval(function () {
+
+    const timer = setInterval(function () {
       if (i <= animationNum && watchedSlide.activeSlide === 2) {
-        number.innerHTML = i + "0".repeat(ostatok.length)
+        number.textContent = i + "0".repeat(staticNum.length)
       } else {
         clearInterval(timer)
       }
@@ -30,7 +24,7 @@ function animationNetwork() {
 
 function closeSection() {
   numbers.forEach((number) => {
-    setTimeout(() => (number.innerHTML = 0), 200)
+    setTimeout(() => (number.textContent = 0), 200)
   })
 }
 
