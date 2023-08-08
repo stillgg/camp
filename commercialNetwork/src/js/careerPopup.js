462fd271bad587cbb0ccb3f7c485f78618fe232b
Makcik303123 <adoberoflit@gmail.com>
2023-08-08 20:05:51 +0300

    add valid

--- commercialNetwork/src/js/careerPopup.js
+++ commercialNetwork/src/js/careerPopup.js
@@ -1,49 +1,91 @@
 const career = document.querySelector(".career")
-const header = document.querySelector(".header")
-const popup = career.querySelector(".career__popup")
+const popup = document.querySelector(".career__popup")
 const buttons = career.querySelectorAll(".main__block")
-const closeBtn = career.querySelector(".close__wrapper")
-const form = career.querySelector(".form")
+const closeBtn = popup.querySelector(".close__wrapper")
+const form = popup.querySelector(".form")
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     popup.classList.add("active")
-    header.classList.add("hidden")
   })
 })
 
 closeBtn.addEventListener("click", () => {
   popup.classList.remove("active")
-  header.classList.remove("hidden")
 })
 
-const inputCity = form.querySelector("#city-select")
-const inputJob = form.querySelector("#job")
-const inputTel = form.querySelector("#tel")
-const inputEmail = form.querySelector("#email")
-const inputName = form.querySelector("#full_name")
-const inputNational = form.querySelector("#nationality")
+const inputEmail = form.querySelector("#Email")
+const inputTel = form.querySelector("#Tel")
+const inputName = form.querySelector("#Name")
+const inputNationality = form.querySelector("#Nationality")
+const inputJob = form.querySelector("#Job")
 
-form.addEventListener("submit", (e) => {
+popup.addEventListener("scroll", (e) => {
   e.preventDefault()
-  console.log(inputCity.value)
-  console.log(inputJob.value)
-})
-popup.addEventListener("wheel", (event) => {
-  event.stopPropagation()
-})
-popup.addEventListener("touchstart", (event) => {
-  event.stopPropagation()
 })
 
-popup.addEventListener("touchend", (event) => {
-  event.stopPropagation()
+const onInput = (input) => {
+  if (!eval(`isValid${input.id}(input.value)`)) {
+    input.classList.add("invalid")
+  } else {
+    input.classList.remove("invalid")
+  }
+}
+
+function isValidEmail(value) {
+  const EMAIL_REGEXP =
+    /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu
+  return EMAIL_REGEXP.test(value)
+}
+
+function isValidName(value) {
+  const regex = /^[A-Za-z ']+$/
+  return value.match(regex) && value.length > 5
+}
+
+function isValidNationality(value) {
+  const regex = /^[A-Za-z]+$/
+  return value.match(regex) && value.length > 3
+}
+
+function isValidJob(value) {
+  const regex = /^[A-Za-z ]+$/
+  return value.match(regex) && value.length > 3
+}
+
+inputEmail.addEventListener("input", function () {
+  onInput(this)
+})
+inputName.addEventListener("input", function () {
+  onInput(this)
+})
+inputNationality.addEventListener("input", function () {
+  onInput(this)
+})
+inputJob.addEventListener("input", function () {
+  onInput(this)
 })
 
-popup.addEventListener("mousedown", (event) => {
-  event.stopPropagation()
-})
+// popup.addEventListener("wheel", (event) => {
+//   event.stopPropagation()
+// })
+// popup.addEventListener("touchstart", (event) => {
+//   event.stopPropagation()
+// })
 
-popup.addEventListener("mouseup", (event) => {
-  event.stopPropagation()
+// popup.addEventListener("touchend", (event) => {
+//   event.stopPropagation()
+// })
+
+// popup.addEventListener("mousedown", (event) => {
+//   event.stopPropagation()
+// })
+
+// popup.addEventListener("mouseup", (event) => {
+//   event.stopPropagation()
+// })
+
+popup.addEventListener("scroll", (e) => {
+  e.preventDefault()
+  console.log(popup)
 })
