60e23f9407b935af4a291af3453a48f941e41cd1
Egor <still2095@gmail.com>
2023-08-12 00:13:20 +0300

    https://github.com/stillgg/camp/issues/105

--- commercialNetwork/src/index.js
+++ commercialNetwork/src/index.js
@@ -1,6 +1,6 @@
-import "./scss/index.scss";
-import "./js/hamburger";
-import "./js/map";
-import "./js/slider";
-import "./js/scrollBar";
-import "./js/networkLoader";
+import "./scss/index.scss"
+import "./js/hamburger"
+import "./js/map"
+import "./js/slider"
+import "./js/scrollBar"
+import "./js/networkLoader"
--- commercialNetwork/src/js/map.js
+++ commercialNetwork/src/js/map.js
@@ -1,14 +1,14 @@
-const marks = document.querySelectorAll(".mark");
+const marks = document.querySelectorAll(".mark")
 
 marks.forEach((mark) => {
   mark.addEventListener("click", () => {
-    deleteClasses();
-    mark.parentNode.classList.add("animation-play");
-  });
-});
+    deleteClasses()
+    mark.parentNode.classList.add("animation-play")
+  })
+})
 
 function deleteClasses() {
   marks.forEach((mark) => {
-    mark.parentNode.classList.remove("animation-play");
-  });
+    mark.parentNode.classList.remove("animation-play")
+  })
 }
