d3254fe4dc6c969bbb00e872d4ff2add8c026be3
mar1cruz <ddovidovich@list.ru>
2023-08-11 19:42:55 +0300

    https://github.com/stillgg/camp/issues/69 fix

--- commercialNetwork/src/index.js
+++ commercialNetwork/src/index.js
@@ -3,4 +3,21 @@ import "./js/hamburger"
 import "./js/map"
 import "./js/teamSlider"
 import "./js/scrollBar"
-import "./js/slider"
+
+import { slider } from "./js/slider"
+
+// const sliderTeam = slider("#slider-team")
+
+const sliderNews = slider("#slider-news")
+
+// const sliderMerch = slider("#slider-merch")
+
+window.addEventListener("resize", () => {
+  // sliderTeam.destroy()
+  sliderNews.destroy()
+  // sliderMerch.destroy()
+
+  // sliderTeam.init()
+  sliderNews.init()
+  // sliderMerch.init()
+})
--- commercialNetwork/src/js/slider.js
+++ commercialNetwork/src/js/slider.js
@@ -5,21 +5,24 @@ function slider(
     gap: 20,
   },
 ) {
+  const defaltParams = {
+    sliderIndex: params?.sliderIndex || 0,
+    gap: params?.gap || 20,
+  }
+
   const area = document.querySelector(selector)
   const btnNext = area.querySelector(".slider-button__right")
   const btnPrev = area.querySelector(".slider-button__left")
   const track = area.querySelector(".slider-track")
   const items = area.querySelectorAll(".slider-item")
-  const getGapTrack = params.gap
-  const itemWidth = items[0].clientWidth + getGapTrack
+  const itemWidth = items[0].clientWidth + defaltParams.gap
   const countActiveSlide = getTotalVisibleSlides(area)
+  const percentShift = 0.2
 
-  let indexActiveSlide = params.sliderIndex
-
+  let indexActiveSlide = defaltParams.sliderIndex
+  console.log(indexActiveSlide)
   let positionStart = 0
-  let currentPosition = 0
   let isDrag = false
-  let resizeTimeout
 
   if (indexActiveSlide === 0) btnPrev.classList.add("hidden")
 
@@ -28,9 +31,6 @@ function slider(
       indexActiveSlide--
       changeSlide(indexActiveSlide)
     }
-    currentPosition = itemWidth * -indexActiveSlide
-
-    flippButtons(indexActiveSlide)
   })
 
   btnPrev.addEventListener("click", () => {
@@ -38,20 +38,7 @@ function slider(
       indexActiveSlide++
       changeSlide(indexActiveSlide)
     }
-
-    currentPosition = itemWidth * -indexActiveSlide
-
-    flippButtons(indexActiveSlide)
   })
-
-  function onDragStart(event) {
-    event.preventDefault()
-    isDrag = true
-
-    positionStart = event.type === "touchstart" ? event.touches[0].clientX : event.clientX
-    positionStart += currentPosition
-    track.style.transitionDuration = "0ms"
-  }
 
   function getTotalVisibleSlides(selector) {
     const sliderWidth = selector.clientWidth
@@ -59,19 +46,9 @@ function slider(
     return Math.floor(sliderWidth / itemWidth)
   }
 
-  function onDragOver(event) {
-    if (!isDrag) return
-
-    const move = event.touches ? event.touches[0].clientX : event.clientX
-
-    track.style.transform = `translate3d(${move - positionStart}px, 0px, 0px)`
-  }
-
   function changeSlide(indexSlide) {
     track.style.transform = `translate3d(${indexSlide * itemWidth}px, 0px, 0px)`
-  }
 
-  function flippButtons(indexSlide) {
     if (indexSlide === 0) {
       btnPrev.classList.add("hidden")
     } else btnPrev.classList.remove("hidden")
@@ -81,90 +58,93 @@ function slider(
     } else btnNext.classList.remove("hidden")
   }
 
+  function onDragStart(event) {
+    event.preventDefault()
+    isDrag = true
+
+    positionStart = event.touches ? event.touches[0].clientX : event.clientX
+
+    track.style.transitionDuration = "0ms"
+  }
+
+  function onDragOver(event) {
+    if (!isDrag) return
+
+    const move = event.touches ? event.touches[0].clientX : event.clientX
+
+    track.style.transform = `translate3d(${move - (positionStart - indexActiveSlide * itemWidth)}px, 0px, 0px)`
+  }
+
   function onDragEnd(event) {
     if (!isDrag) return
 
-    let positionEnd = event.type === "touchend" ? event.changedTouches[0].clientX : event.clientX
-
-    positionEnd += currentPosition
-
-    const percentShift = 0.2
-    const isMoved = positionEnd - positionStart > itemWidth * percentShift
+    const positionEnd = event.touches ? event.changedTouches[0].clientX : event.clientX
+    const isMoved = Math.abs(positionEnd - positionStart) > itemWidth * percentShift
     const isFirstSlide = indexActiveSlide !== 0
+    const isLastSlide = Math.abs(indexActiveSlide) !== items.length - countActiveSlide
 
-    if (
-      positionStart > positionEnd &&
-      Math.abs(positionEnd - positionStart) > itemWidth * percentShift &&
-      Math.abs(indexActiveSlide) !== items.length - countActiveSlide
-    ) {
+    if (positionStart > positionEnd && isMoved && isLastSlide) {
       indexActiveSlide--
     }
 
     if (positionStart < positionEnd && isMoved && isFirstSlide) {
       indexActiveSlide++
     }
-
-    flippButtons(indexActiveSlide)
     changeSlide(indexActiveSlide)
 
     track.style.transitionDuration = "400ms"
-
-    currentPosition = -(indexActiveSlide * itemWidth)
     isDrag = false
   }
 
-  function checkExit() {
+  function onMouseLeave() {
     changeSlide(indexActiveSlide)
     isDrag = false
     track.style.transitionDuration = "600ms"
   }
 
-  function clearSlider() {
-    window.removeEventListener("resize", debounceResize)
-    document.removeEventListener("mouseleave", checkExit)
-
-    track.removeEventListener("mousedown", onDragStart)
-    document.removeEventListener("mousemove", onDragOver)
-    document.removeEventListener("mouseup", onDragEnd)
-
-    track.removeEventListener("touchstart", onDragStart)
-    document.removeEventListener("touchmove", onDragOver)
-    document.removeEventListener("touchend", onDragEnd)
-  }
-
-  function callSlider() {
-    params = {
-      gap: 20,
-      sliderIndex: indexActiveSlide,
+  function recalculationActiveSlide(indexActiveSlide) {
+    // const isLastSlide = Math.abs(indexActiveSlide) === items.length - countActiveSlide
+    console.log(indexActiveSlide)
+    // while (isLastSlide && Math.abs(indexActiveSlide) > lastSlide) {
+    //   indexActiveSlide++
+    // }
+    while (indexActiveSlide) {
+      indexActiveSlide++
     }
-    slider(selector, params)
   }
 
-  function updateSlider() {
-    clearSlider()
-    callSlider()
+  function beforeInitialization() {
+    changeSlide(indexActiveSlide)
+    document.addEventListener("mouseleave", onMouseLeave)
+
+    track.addEventListener("mousedown", onDragStart)
+    document.addEventListener("mousemove", onDragOver)
+    document.addEventListener("mouseup", onDragEnd)
+
+    track.addEventListener("touchstart", onDragStart)
+    document.addEventListener("touchmove", onDragOver)
+    document.addEventListener("touchend", onDragEnd)
   }
 
-  function debounceResize() {
-    clearTimeout(resizeTimeout)
-    resizeTimeout = setTimeout(updateSlider, 200)
+  beforeInitialization()
+
+  return {
+    destroy: function () {
+      document.removeEventListener("mouseleave", onMouseLeave)
+
+      track.removeEventListener("mousedown", onDragStart)
+      document.removeEventListener("mousemove", onDragOver)
+      document.removeEventListener("mouseup", onDragEnd)
+
+      track.removeEventListener("touchstart", onDragStart)
+      document.removeEventListener("touchmove", onDragOver)
+      document.removeEventListener("touchend", onDragEnd)
+    },
+    init: function () {
+      console.log(indexActiveSlide)
+      slider(selector, { ...defaltParams, sliderIndex: indexActiveSlide })
+    },
   }
-
-  window.addEventListener("resize", debounceResize)
-
-  document.addEventListener("mouseleave", checkExit)
-
-  track.addEventListener("mousedown", onDragStart)
-  document.addEventListener("mousemove", onDragOver)
-  document.addEventListener("mouseup", onDragEnd)
-
-  track.addEventListener("touchstart", onDragStart)
-  document.addEventListener("touchmove", onDragOver)
-  document.addEventListener("touchend", onDragEnd)
 }
 
-slider("#slider-merch")
-
-slider("#slider-team")
-
-slider("#slider-news")
+export { slider }
