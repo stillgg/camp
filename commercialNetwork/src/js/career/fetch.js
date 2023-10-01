65a94dc9573a38910a7bbc4259443c65e551c530
mar1cruz <ddovidovich@list.ru>
2023-10-01 23:56:36 +0300

    https://github.com/stillgg/camp/issues/77

--- commercialNetwork/src/js/career/fetch.js
+++ commercialNetwork/src/js/career/fetch.js
@@ -1,26 +1,24 @@
-const requestURL = 'https://jsonplaceholder.typicode.com/users'
+const requestURL = "https://jsonplaceholder.typicode.com/users"
 
-function sendRequest(method,url,body = null) {
-    const headers = {
-        'Content-Type' : 'application/json'
+function sendRequest(method, url, body = {}) {
+  const headers = {
+    "Content-Type": "application/json",
+  }
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(body),
+    headers: headers,
+  }).then((Response) => {
+    if (Response.ok) {
+      return Response.json()
     }
-    return fetch(url,{
-        method:method,
-        body:JSON.stringify(body),
-        headers:headers
-    }).then(Response =>{
-        if(Response.ok){
-            return Response.json()
-        }
 
-        return Response.json().then(Error => {
-            const e = new Error('ошибка')
-            e.data = Error
-            throw e
-        })
+    return Response.json().then((Error) => {
+      const e = new Error("ошибка")
+      e.data = Error
+      throw e
     })
+  })
 }
 
-
-
-export {requestURL,sendRequest}
+export { requestURL, sendRequest }
--- commercialNetwork/src/js/career/index.js
+++ commercialNetwork/src/js/career/index.js
@@ -14,15 +14,20 @@ import {
 } from "../validation/validators"
 import "./events"
 import "./fillCity"
-import './fetch'
-import { sendRequest,requestURL } from "./fetch"
+import "./fetch"
+import { sendRequest, requestURL } from "./fetch"
 
 const form = document.querySelector("#form")
 
 const btnSubmit = form.querySelector("#submit")
-
 const buttons = document.querySelectorAll(".main__block")
-const closeBtn = document.querySelector(".close__wrapper")
+const successPopup = document.querySelector(".popup__form")
+const closeSuccessPopup = document.querySelector("#success-popup-form")
+const career__popup = document.querySelector(".career__popup")
+const inputs = form.querySelectorAll("input")
+const closeBtn = document.querySelector("#close-popup")
+const select = form.querySelector("#city")
+const fileConainer = form.querySelector("#fileContainer")
 
 const schema = {
   agreements: agreementsValidator,
@@ -67,10 +72,151 @@ form.addEventListener("submit", (e) => {
   e.preventDefault()
 
   if (btnSubmit.classList.contains("disabled")) return
-
   v.validateAll()
 
-  sendRequest('POST',requestURL,v.elements).then(data => console.log(data))
+  if (!v.isError) {
+    const btnText = btnSubmit.textContent
+    btnSubmit.innerHTML = `<span class='loader'></span>`
+
+    const loader = form.querySelector(".loader")
+
+    loader.classList.add("active")
+
+    setTimeout(() => {
+      successPopup.classList.add("active")
+      loader.classList.add("active")
+      btnSubmit.textContent = btnText
+      career__popup.classList.add("blur")
+    }, 2000)
+  }
+  // sendRequest("POST", requestURL).then((data) => console.log(data))
 })
+
+closeSuccessPopup.addEventListener("click", (e) => {
+  if (!e.target) return
+
+  successPopup.classList.remove("active")
+  career__popup.classList.remove("blur")
+
+  inputs.forEach((input) => {
+    input.value = ""
+  })
+
+  select.value = ""
+  fileConainer.childNodes[0].remove()
+})
+
+closeBtn.addEventListener("click", () => {
+  career__popup.classList.remove("active")
+})
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const workName = button.querySelector(".content__title").textContent
+
+    addDescritionWork(workName)
+
+    career__popup.classList.add("active")
+  })
+})
+
+function addDescritionWork(workName) {
+  const description = document.querySelector(".preview__text")
+  const inputJob = form.querySelector("#job")
+
+  inputJob.value = workName.toLowerCase()
+
+  switch (workName) {
+    case "ПОЛЕВОЙ ТРЕНЕР ПО ПРЯМЫМ ПРОДАЖАМ":
+      descriptionTrainer(description, workName)
+      break
+
+    case "ПРОМОУТЕР":
+      descriptionPromoter(description, workName)
+      break
+
+    case "МЕНЕДЖЕР ПО РАЗВИТИЮ":
+      descriptionManager(description, workName)
+      break
+
+    case "СУПЕРВАЙЗЕР":
+      descriptionSupervaiser(description, workName)
+      break
+  }
+}
+
+function descriptionTrainer(description, workName) {
+  description.innerHTML = `<h2 class="text__title">${workName}</h2>
+  <span class="text__requirements">Что мы ждём:</span>
+  <div class="requirements__description">
+    <p class="description__paragraph">
+    Опыт работы в активных продажах обязателен;
+    </p>
+    <p class="description__paragraph">Опыт обучения персонала розничных торговых сетей от 2 лет;</p>
+    <p class="description__paragraph">Опыт разработки и внедрения скриптов;</p>
+    <p class="description__paragraph">Готовность к постоянным командировкам (до 90% рабочего времени);</p>
+    <p class="description__paragraph">Водительские права категории B — желательно, но не обязательно.</p>
+  </div>
+  <span class="text__requirements">Что мы обещаем:</span>
+  <div class="requirements__description">
+    <p class="description__paragraph">
+    Заработная плата — от 70 000 ₽ + компенсационный пакет (командировки, суточные и т.д.).
+    </p>
+  </div>`
+}
+
+function descriptionPromoter(description, workName) {
+  description.innerHTML = `<h2 class="text__title">${workName}</h2>
+  <span class="text__requirements">Что мы ждём:</span>
+  <div class="requirements__description">
+    <p class="description__paragraph">
+    Вам от 14 лет;
+    </p>
+    <p class="description__paragraph">Возможно без опыта;</p>
+    <p class="description__paragraph">Активность и целеустремлённость.</p>
+  <span class="text__requirements">Что мы обещаем:</span>
+  <div class="requirements__description">
+    <p class="description__paragraph">
+    Заработная плата 15 000 - 70 000 ₽ (в зависимости от занятости).
+    </p>
+  </div>`
+}
+
+function descriptionManager(description, workName) {
+  description.innerHTML = `<h2 class="text__title">${workName}</h2>
+  <span class="text__requirements">Что мы ждём:</span>
+  <div class="requirements__description">
+    <p class="description__paragraph">
+    Опыт работы в сфере развития розничного ритейла обязателен;
+    </p>
+    <p class="description__paragraph">Опыт заключения арендных договоров с торговыми центрами, магазинами, гипермаркетами;</p>
+    <p class="description__paragraph">Опыт ведения переговоров с ЛПР;</p>
+    <p class="description__paragraph">Готовность к постоянным командировкам (до 90% рабочего времени);</p>
+    <p class="description__paragraph">Водительские права категории B — желательно, но не обязательно.</p>
+  </div>
+  <span class="text__requirements">Что мы обещаем:</span>
+  <div class="requirements__description">
+    <p class="description__paragraph">
+    Заработная плата — от 70 000 ₽ + компенсационный пакет (командировки, суточные и т.д.).
+    </p>
+  </div>`
+}
+
+function descriptionSupervaiser(description, workName) {
+  description.innerHTML = `<h2 class="text__title">${workName}</h2>
+  <span class="text__requirements">Что мы ждём:</span>
+  <div class="requirements__description">
+    <p class="description__paragraph">Вам от 18 до 30 лет;</p>
+    <p class="description__paragraph">Опыт активных продаж от 1 года;</p>
+    <p class="description__paragraph">Водительские права категории B — желательно, но не обязательно;</p>
+    <p class="description__paragraph">Готовность к релокации.</p>
+  </div>
+  <span class="text__requirements">Что мы обещаем:</span>
+  <div class="requirements__description">
+    <p class="description__paragraph">
+    Заработная плата 40 000 - 200 000 ₽ + оплата жилья (в зависимости от объёма контролируемой сети).
+    </p>
+  </div>`
+}
 
 export { v }
