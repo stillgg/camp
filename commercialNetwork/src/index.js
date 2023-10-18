469cde25bb4e0ab93a14ac5ef512b311e4a2dae8
mar1cruz <ddovidovich@list.ru>
2023-10-18 22:15:51 +0300

    https://github.com/stillgg/camp/issues/40

--- commercialNetwork/src/index.js
+++ commercialNetwork/src/index.js
@@ -3,6 +3,7 @@ import "./js/hamburger"
 import "./js/map"
 import "./js/scrollBar"
 import "./js/networkLoader"
+import './js/index'
 
 import "./js/career/index"
 import { slider } from "./js/slider"
--- commercialNetwork/src/js/index.js
+++ commercialNetwork/src/js/index.js
@@ -1,3 +1,12 @@
-document.addEventListener('orientationchange',(e)=>{
-console.log(1)
+
+const portainPopup = document.querySelector('.popup__portain-mode')
+const mobileWidth = 768
+
+screen.orientation.addEventListener('change',(e)=>{
+    portainPopup.classList.remove('active')
+
+    if(e.target.type !== 'portrait-primary'){
+        portainPopup.classList.add('active')
+    }
+
 })
