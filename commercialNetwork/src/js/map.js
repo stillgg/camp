17877f5944d0bf6afecf03fa4b97acee2d7ea9df
mar1cruz <ddovidovich@list.ru>
2023-10-18 20:11:26 +0300

    https://github.com/stillgg/camp/issues/48

--- commercialNetwork/src/js/map.js
+++ commercialNetwork/src/js/map.js
@@ -1,4 +1,7 @@
 const marks = document.querySelectorAll(".mark")
+const bcgMap = document.querySelector('.background-map-wrapper')
+const mapPopup = document.querySelector('.map__popup')
+const mapClose = document.querySelector('.map__close')
 
 marks.forEach((mark) => {
   mark.addEventListener("click", () => {
@@ -12,3 +15,11 @@ function deleteClasses() {
     mark.parentNode.classList.remove("animation-play")
   })
 }
+
+bcgMap.addEventListener('click',()=>{
+  mapPopup.classList.add('active')
+})
+
+mapClose.addEventListener('click',()=> {
+  mapPopup.classList.remove('active')
+})
