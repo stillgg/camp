80a5c08711ca352395415bfdecce1f76d1305c4d
mar1cruz <ddovidovich@list.ru>
2023-10-23 20:09:15 +0300

    https://github.com/stillgg/camp/issues/40

--- commercialNetwork/src/js/index.js
+++ commercialNetwork/src/js/index.js
@@ -1,11 +1,33 @@
+// const portainPopup = document.querySelector('.popup__portain-mode')
 
-const portainPopup = document.querySelector('.popup__portain-mode')
+// window.addEventListener('resize', changeLandscape)
 
-screen.orientation.addEventListener('change',(e)=>{
-    portainPopup.classList.remove('active')
+// function changeLandscape() {
+//     const isMobile = /iPhone|iPad|iPod|Android|Mobile|BlackBerry|IEMobile|Opera Mini/i.test(window.navigator.userAgent);
+//     console.log(isMobile)
 
-    if(e.target.type !== 'portrait-primary'){
-        portainPopup.classList.add('active')
-    }
+// screen.orientation.addEventListener('change',(e)=>{
+//     portainPopup.classList.remove('active')
+//     console.log(e.target.type)
+//     if(e.target.type !== 'portrait-primary' && isMobile){
+//         portainPopup.classList.add('active')
+//     }
 
+// })
+// }
+
+const portainPopup = document.querySelector(".popup__portain-mode")
+let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgentData.mobile)
+
+window.addEventListener("resize", () => {
+  isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgentData.mobile)
+})
+
+screen.orientation.addEventListener("change", (e) => {
+  portainPopup.classList.remove("active")
+  console.log(e.target.type !== "portrait-primary")
+  console.log("isMobile", isMobile)
+  if (e.target.type !== "portrait-primary" && isMobile) {
+    portainPopup.classList.add("active")
+  }
 })
