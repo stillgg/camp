af2ad6c2051f7834a58a2311ca338e9b7482c340
dmitry <ddovidovich@list.ru>
2023-11-04 14:50:54 +0300

    https://github.com/stillgg/camp/issues/48

--- commercialNetwork/src/js/map.js
+++ commercialNetwork/src/js/map.js
@@ -2,7 +2,7 @@ const marks = document.querySelectorAll(".mark")
 const bcgMap = document.querySelector(".background-map-wrapper")
 const mapPopup = document.querySelector(".map__popup-wrapper")
 const mapClose = document.querySelector(".map__popup-close")
-const isMobile = window.innerWidth <= 992
+const arrows = document.querySelector('.arrows')
 
 marks.forEach((mark) => {
   mark.addEventListener("click", () => {
@@ -17,12 +17,15 @@ function deleteClasses() {
   })
 }
 
+
+
+
+
 bcgMap.addEventListener("click", () => {
-  if (isMobile) {
-    mapPopup.classList.add("active")
-  }
+  mapPopup.classList.add("active")
 })
 
 mapClose.addEventListener("click", () => {
   mapPopup.classList.remove("active")
 })
+
