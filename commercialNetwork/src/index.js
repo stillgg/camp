c53338fc2a24921c8ecdbb4c3d78965fcdd01b2f
dmitry <ddovidovich@list.ru>
2023-11-15 20:42:59 +0300

    https://github.com/stillgg/camp/issues/78

--- commercialNetwork/src/index.js
+++ commercialNetwork/src/index.js
@@ -1,11 +1,11 @@
 import "./scss/index.scss"
+import "./js/preloader"
 import "./js/hamburger"
 import "./js/map"
 import "./js/scrollBar"
 import "./js/networkLoader"
 import "./js/yandexMap"
 import "./js/career/index"
-import "./js/preloader"
 
 import { slider } from "./js/slider"
 
