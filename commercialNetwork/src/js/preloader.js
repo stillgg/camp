a65351e5f6544d04889e7e20635b6a71346b9258
Egor <still2095@gmail.com>
2023-11-21 00:24:23 +0300

    https://github.com/stillgg/camp/issues/78

--- commercialNetwork/src/js/preloader.js
+++
@@ -1,7 +0,0 @@
-const preloader = document.querySelector(".preloader")
-
-document.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    preloader.classList.remove("active")
-  }, 5000)
-})
