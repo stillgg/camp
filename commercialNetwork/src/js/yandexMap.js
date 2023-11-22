531bfe79aca99a535d7b2d702c70770d1c97626e
dmitry <ddovidovich@list.ru>
2023-11-22 22:19:45 +0300

    https://github.com/stillgg/camp/issues/145

--- commercialNetwork/src/js/yandexMap.js
+++ commercialNetwork/src/js/yandexMap.js
@@ -2,6 +2,7 @@ const popup = document.querySelector(".popup__yandexMap")
 const openPopup = document.querySelector(".content__map")
 const closePopup = popup.querySelector(".preview__close")
 
+
 openPopup.addEventListener("click", () => {
   popup.classList.add("active")
 })
