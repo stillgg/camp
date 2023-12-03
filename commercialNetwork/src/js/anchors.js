ea47aabe16360fcd5cb8a8f03bcac8ef7e35765f
dmitry <ddovidovich@list.ru>
2023-12-03 22:49:48 +0300

    https://github.com/stillgg/camp/issues/166

--- commercialNetwork/src/js/anchors.js
+++ commercialNetwork/src/js/anchors.js
@@ -2,7 +2,6 @@ import { setSlide, getSectionIndex } from "./scrollBar"
 
 const sections = document.querySelectorAll("section")
 const anchors = document.querySelectorAll("a[data-anchor], .header__logo")
-console.log(anchors)
 
 anchors.forEach((link) => {
   link.addEventListener("click", () => {
