2ffd85067ac3f4163ededb5416496452447bc07c
dmitry <ddovidovich@list.ru>
2023-12-09 14:21:12 +0300

    https://github.com/stillgg/camp/pull/178

--- commercialNetwork/src/js/slider.js
+++ commercialNetwork/src/js/slider.js
@@ -145,11 +145,13 @@ function slider(
     btnPrev.addEventListener("click", onClickBtnLeft)
 
     if (defaultParams.effect === "default") {
-      track.addEventListener("mousedown", onDragStart)
+      track.addEventListener("mousedown", onDragStart, { passive: true })
       document.addEventListener("mousemove", onDragOver)
       document.addEventListener("mouseup", onDragEnd)
 
-      track.addEventListener("touchstart", onDragStart)
+      track.addEventListener("touchstart", onDragStart, {
+        passive: true,
+      })
       document.addEventListener("touchmove", onDragOver)
       document.addEventListener("touchend", onDragEnd)
 
